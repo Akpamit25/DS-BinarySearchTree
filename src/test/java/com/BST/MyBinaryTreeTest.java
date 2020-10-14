d4dced15a5f264229735821c04c1ec032cb279fb
Amit Kumar <amitnitsri25@gmail.com>
2020-10-14 09:22:08 +0530

    BST Created

--- src/test/java/com/BST/MyBinaryTreeTest.java
+++ src/test/java/com/BST/MyBinaryTreeTest.java
@@ -1,5 +1,23 @@
 package com.BST;
 
-public class MyBinaryTreeTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyBinaryTreeTest {
+
+	@Test
+	void given3Numbers_WhenAdded_ShouldFormABinaryTree() {
+		MyBinaryTree<Integer> myTree = new MyBinaryTree<Integer>();
+		myTree.add(56);
+		myTree.add(30);
+		myTree.add(70);
+		
+		boolean result = myTree.root.getKey().equals(56)
+						&& myTree.root.getLeft().getKey().equals(30)
+						&& myTree.root.getRight().getKey().equals(70);
+						
+		assertTrue(result);//
+	}
 
 }
